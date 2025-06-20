f8fe7ff54350367a205a6c800aa706b30daa3f66
Théo Tyburn <theo.tyburn@posteo.net>
2025-06-20 10:47:45 +0200

    products: allow any arity

--- ctmdp/products.py
+++ ctmdp/products.py
@@ -1,4 +1,5 @@
 from ctmdp.mdp import MDP
+from functools import reduce, partial
 
 
 def _bp_action_data(states, labels, index):
@@ -30,7 +31,7 @@ def _bp_action_data(states, labels, index):
     }
 
 
-def box_product(M1, M2, labels=["M1", "M2"]):
+def _box_product(M1, M2, labels=["M1", "M2"]):
     desc = {
         (s1.label, s2.label): _bp_action_data((s1, s2), labels, 0)
         | _bp_action_data((s1, s2), labels, 1)
@@ -48,7 +49,7 @@ def product_measure(a1_measure, a2_measure):
     }
 
 
-def cartesian_product(M1, M2, sep="-"):
+def _cartesian_product(M1, M2, sep="-"):
     desc = {
         (s1.label, s2.label): {
             f"{a1.label}{sep}{a2.label}": product_measure(a1.measure, a2.measure)
@@ -59,3 +60,55 @@ def cartesian_product(M1, M2, sep="-"):
         for s2 in M2.states.values()
     }
     return MDP(desc)
+
+
+# Relabeling function
+def _append(args):
+    l = args[0]
+    e = args[1]
+    return tuple([*l, e])
+
+
+def _relabel(product):
+    "Adds relabeling to a product operation."
+
+    def f(*args, **kwargs):
+        mdp = product(*args, **kwargs)
+        mdp.relabel_all_states(_append)
+        return mdp
+
+    return f
+
+
+def cartesian_product(*mdps, sep="-"):
+    match len(mdps):
+        case 1:
+            return mdps[0]
+        case 2:
+            return _cartesian_product(*mdps, sep=sep)
+        case _:
+            tail = _cartesian_product(*mdps[-2::], sep=sep)
+            body = mdps[:-2]
+            return reduce(_relabel(_cartesian_product), body, tail)
+
+
+def _n_ary_product(product):
+    "Make a binary operation into a n-ary operation."
+
+    def n_ary_product(*mdps, **kwargs):
+        _product = partial(product, **kwargs)
+        match len(mdps):
+            case 1:
+                return mdps[0]
+            case 2:
+                return _product(*mdps)
+            case _:
+                tail = _product(*mdps[-2::])
+                body = mdps[:-2]
+                return reduce(_relabel(_product), body, tail)
+
+    return n_ary_product
+
+
+cartesian_product = _n_ary_product(_cartesian_product)
+box_product = _n_ary_product(_box_product)
