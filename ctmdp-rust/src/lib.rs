cea92fa57f5940a73bb772a34e026376c79bbcbd
Tanner Duve <tannerduve@gmail.com>
2025-11-13 17:31:17 -0800

    formatting

--- ctmdp-rust/src/lib.rs
+++ ctmdp-rust/src/lib.rs
@@ -4,9 +4,9 @@ pub mod gridworld;
 pub mod mdp;
 pub mod mdp_trait;
 pub mod measure;
+pub mod pathmdp;
 pub mod products;
 pub mod q_learning;
-pub mod pathmdp;
 
 const NO_OP_TRANSITION_REWARD: f64 = -1.0;
 const END_TRANSITION_REWARD: f64 = 10.0;
--- ctmdp-rust/src/pathmdp.rs
+++ ctmdp-rust/src/pathmdp.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashMap, hash::Hash, path::Path, vec};
-use madepro::models::{Sampler, State, Action};
-use crate::{mdp_trait::MDP, measure::Probability};
-use crate::measure::Measure;
 use super::{END_TRANSITION_REWARD, NO_OP_TRANSITION_REWARD};
+use crate::measure::Measure;
+use crate::{mdp_trait::MDP, measure::Probability};
+use madepro::models::{Action, Sampler, State};
+use std::{collections::HashMap, hash::Hash, path::Path, vec};
 
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
 pub enum PathAction {
@@ -16,7 +16,7 @@ impl Action for PathAction {}
 pub struct PathState(pub(crate) usize);
 
 impl PathState {
-    pub fn new (idx : usize) -> Self {
+    pub fn new(idx: usize) -> Self {
         PathState(idx)
     }
 }
@@ -30,21 +30,19 @@ pub struct PathWorld {
 }
 
 impl PathWorld {
-    pub fn new(
-        states: Vec<PathState>,
-        actions: Vec<PathAction>,
-    ) -> Self {
-        PathWorld { states: states.into(), actions: actions.into() }
+    pub fn new(states: Vec<PathState>, actions: Vec<PathAction>) -> Self {
+        PathWorld {
+            states: states.into(),
+            actions: actions.into(),
+        }
     }
     // pub fn cyclic_new(length: usize) -> Self {
     //     PathWorld { length, actions: vec![PathAction::Next, PathAction::Prev].into(), cyclic: true }
-    // }    
+    // }
     pub fn length(&self) -> usize {
-        self.states.iter().count()  // Compute from states
+        self.states.iter().count() // Compute from states
     }
 }
-
-
 
 impl MDP for PathWorld {
     type State = PathState;
@@ -62,37 +60,31 @@ impl MDP for PathWorld {
     }
 
     fn stochastic_transition(
-            &self,
-            state: &Self::State,
-            action: &Self::Action,
-        ) -> (Measure<Self::State>, f64) {
-            let current = state.0;
-            let length = self.length();
-            // Tentative position
-            let next = match action {
-                Self::Action::Next => current + 1,
-                Self::Action::Prev => current.saturating_sub(1),
-            };
-            // Out of bounds check
-            if next >= length || next == current  {
-                let measure = Measure::deterministic(state.clone());
-                return (measure, NO_OP_TRANSITION_REWARD);
-            } 
-            let reward = match action {
-                Self::Action::Next => 0.1,
-                Self::Action::Prev => -0.5,
-            }; 
-            let measure = Measure::deterministic(PathState(next));
-            if next == length - 1 {
-                (measure, END_TRANSITION_REWARD + reward)
-            } else {
-                (measure, reward)
-            }
+        &self,
+        state: &Self::State,
+        action: &Self::Action,
+    ) -> (Measure<Self::State>, f64) {
+        let current = state.0;
+        let length = self.length();
+        // Tentative position
+        let next = match action {
+            Self::Action::Next => current + 1,
+            Self::Action::Prev => current.saturating_sub(1),
+        };
+        // Out of bounds check
+        if next >= length || next == current {
+            let measure = Measure::deterministic(state.clone());
+            return (measure, NO_OP_TRANSITION_REWARD);
+        }
+        let reward = match action {
+            Self::Action::Next => 0.1,
+            Self::Action::Prev => -0.5,
+        };
+        let measure = Measure::deterministic(PathState(next));
+        if next == length - 1 {
+            (measure, END_TRANSITION_REWARD + reward)
+        } else {
+            (measure, reward)
+        }
     }
-
-
-
 }
-
-
-
